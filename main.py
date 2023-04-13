9a1cca372206741a367281002da5577ba48fc7ce
Artem Urvanov <temaptz@gmail.com>
2023-04-13 11:29:39 +0300

    getting data

--- main.py
+++ main.py
@@ -16,37 +16,37 @@ bottom_left_lat = coordinates_left_bottom[1]
 lon_iterator = 0
 lat_iterator = 0
 
-# while bottom_left_lat < source_coordinates['top_right_lat']:
-#     while bottom_left_lon < coordinates_right_top[0]:
-#         print('Will take screenshot with bounds', bottom_left_lon, screenshot.get_bounds_by_points(
-#             bottom_left_lon=bottom_left_lon,
-#             bottom_left_lat=bottom_left_lat,
-#             top_right_lon=bottom_left_lon + delta_lon,
-#             top_right_lat=bottom_left_lat + delta_lat
-#         ))
-#
-#         correct_bounds = screenshot.take(
-#             bottom_left_lon=bottom_left_lon,
-#             bottom_left_lat=bottom_left_lat,
-#             top_right_lon=bottom_left_lon + delta_lon,
-#             top_right_lat=bottom_left_lat + delta_lat,
-#             lon_number=lon_iterator,
-#             lat_number=lat_iterator
-#         )
-#
-#         # print('Taken screenshot with bounds', correct_bounds)
-#
-#         lon_iterator += 1
-#         correct_points = screenshot.get_points_by_bounds(correct_bounds)
-#         print('SCREEN TAKEN:', bottom_left_lon, (bottom_left_lon + delta_lon))
-#         print('DELTA LON:', (bottom_left_lon - correct_points['bottom_left_lon']))
-#         # bottom_left_lon = correct_points['bottom_left_lon']
-#         # bottom_left_lat = correct_points['bottom_left_lat']
-#         bottom_left_lon += delta_lon
-#
-#     lon_iterator = 0
-#     lat_iterator += 1
-#     bottom_left_lon = coordinates_left_bottom[0]
-#     bottom_left_lat += delta_lat
+while bottom_left_lat < source_coordinates['top_right_lat']:
+    while bottom_left_lon < coordinates_right_top[0]:
+        print('Will take screenshot with bounds', bottom_left_lon, screenshot.get_bounds_by_points(
+            bottom_left_lon=bottom_left_lon,
+            bottom_left_lat=bottom_left_lat,
+            top_right_lon=bottom_left_lon + delta_lon,
+            top_right_lat=bottom_left_lat + delta_lat
+        ))
+
+        correct_bounds = screenshot.take(
+            bottom_left_lon=bottom_left_lon,
+            bottom_left_lat=bottom_left_lat,
+            top_right_lon=bottom_left_lon + delta_lon,
+            top_right_lat=bottom_left_lat + delta_lat,
+            lon_number=lon_iterator,
+            lat_number=lat_iterator
+        )
+
+        # print('Taken screenshot with bounds', correct_bounds)
+
+        lon_iterator += 1
+        correct_points = screenshot.get_points_by_bounds(correct_bounds)
+        print('SCREEN TAKEN:', bottom_left_lon, (bottom_left_lon + delta_lon))
+        print('DELTA LON:', (bottom_left_lon - correct_points['bottom_left_lon']))
+        # bottom_left_lon = correct_points['bottom_left_lon']
+        # bottom_left_lat = correct_points['bottom_left_lat']
+        bottom_left_lon += delta_lon
+
+    lon_iterator = 0
+    lat_iterator += 1
+    bottom_left_lon = coordinates_left_bottom[0]
+    bottom_left_lat += delta_lat
 
 images.concat(10)
